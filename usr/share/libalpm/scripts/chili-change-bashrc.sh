4002c7d522f69d48c6b4f0577c8021d13106c3b7
vcatafesta <vcatafesta@gmail.com>
2024-12-04 20:32:14 -0400

    build-package release: qua 04 dez 2024 20:32:14 -04 Vilmar Catafesta (vcatafesta@gmail.com)

--- usr/share/libalpm/scripts/chili-change-bashrc.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-# Replacing the original /etc/skel/.bashrc file with the modified file
-cFile='/etc/skel/.bashrc'
-cbakFile='/etc/skel/.bashrc.bak'
-
-if [ -e "$cFile" ]; then
-	mv -f "$cFile" "$cbakFile"
-fi
